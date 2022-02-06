a1af769f48113365f7087540181ef546d92b55d5
sorawich-s63 <s6301012630176@email.kmutnb.ac.th>
2022-02-06 22:36:15 +0700

    Text inputs

--- cypress/integration/input-form.spec.js
+++ cypress/integration/input-form.spec.js
@@ -1,5 +1,18 @@
-describe('Input from', () => {
-    it('focuses input on load', () => {
-        cy.visit('http://localhost:3030')
+describe('Input form', () => {
+    beforeEach(() => {
+      cy.visit('/')
     })
-})
+  
+    it('focuses input on load', () => {
+      cy.focused()
+        .should('have.class', 'new-todo')
+    })
+  
+    it('accepts input', () => {
+      const typedText = 'Buy Milk'
+  
+      cy.get('.new-todo')
+        .type(typedText)
+        .should('have.value', typedText)
+    })
+  })
